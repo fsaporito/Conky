46a1be7b35bb0b618167819a624eae6f9a5d11c7
fsaporito <sap93@gmail.com>
2022-02-19 00:35:51 +0100

    Add conky configs for bar information

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!bin/bash
 cp -p "$HOME/conky.conf" "$HOME/conky.conf.bkp"
-cp -p conky.conf "$HOME/conky.conf"
+cp -p conky_left_1.conf "$HOME/conky_left_1.conf"
+cp -p conky_left_2.conf "$HOME/conky_left_2.conf"
+cp -p conky_right.conf "$HOME/conky_right.conf"
--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!bin\bash
 
-conky -c conky.conf
+conky -c conky.conf &
+conky -c conky_left_1.conf &
+conky -c conky_left_2.conf &
+conky -c conky_right.conf &
